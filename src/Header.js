17b5cc1e151ac577de9dda9c01d40da5eee880d6
Joe Attardi <jattardi@gmail.com>
2019-12-09 20:02:27 -0500

    Add Font Awesome, tweak header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header id={styles.header}>
+      <FontAwesomeIcon icon={faChartLine} size="2x" />
       <h1>npm dashboard</h1>
     </header>
   )
