0252c94af02fa0e8c73f5bbe1e044b75864c2a20
Joe Attardi <jattardi@gmail.com>
2019-12-10 23:49:19 -0500

    Use CSS Grid

--- src/Package.js
+++ src/Package.js
@@ -27,14 +27,14 @@ export default function Package({ data, downloads }) {
           <div className={styles.date}>{formatDistanceToNow(new Date(metadata.date), { addSuffix: true })}</div>
           {metadata.author ? <div className={styles.avatar} data-tip={metadata.author.name}><Gravatar email={metadata.author.email} size={25} /></div> : null}
         </div>
-        <div className={styles['section-title']}><FontAwesomeIcon icon={faStar} /> Score</div>
+        <div className={styles['section-title']}><FontAwesomeIcon size="sm" icon={faStar} /> <span>Score</span></div>
         <div className={styles.scores}>
           <Score score={score.detail.quality} name="Quality" label="Q" />
           <Score score={score.detail.popularity} name="Popularity" label="P" />
           <Score score={score.detail.maintenance} name="Maintenance" label="M" data-tip="shit" />
           <OverallScore score={score.final} />
         </div>
-        <div className={styles['section-title']}><FontAwesomeIcon icon={faCloudDownloadAlt} /> Downloads</div>
+        <div className={styles['section-title']}><FontAwesomeIcon size="sm" icon={faCloudDownloadAlt} /> <span>Downloads</span></div>
         <div className={styles.downloads}>
           {downloads && downloads.lastWeek ? <DownloadCount label="Last week" count={downloads.lastWeek} /> : null}
           <div style={{ flexGrow: 1 }}></div>
